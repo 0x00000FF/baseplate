9beef444e6992389fbcfee3143960861dc89b942
Kangjun Heo <kheo@sidelcorp.com>
2023-12-07 01:12:39 +0900

    VBR template

--- loader/vbr.asm
+++ loader/vbr.asm
@@ -1,7 +1,24 @@
 [BITS 16]
-ORG 0x7C00
+[ORG 0x7C00]
 
-jmp 0xFFFF:0h
+vbr_main:
+    jmp $
+
+    ; check A20 line and enable
+
+    ; read kernel loader
+
+    ; jump to loader
+    jmp 0x1000:0
+
+vbr_fault:
+    jmp 0xffff:0
+
+__DATA_VARS:
+
+__DATA_STR:
 
 times 510 - ($ - $$) db 0x00
-db 0x55, 0xAA
+
+__MAGIC:
+    db 0x55, 0xAA
